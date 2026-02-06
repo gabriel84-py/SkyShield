946a441dc7a531f506dd2eda052d31025a4d9ae7
gabriel84-py <hexahack.g8430@gmail.com>
2026-02-06 21:47:42 +0100

    motors commit

--- testmotor.py
+++ testmotor.py
@@ -2,16 +2,28 @@ from esc_motor import ESC
 import time
 
 # init moteur GPIO 2
-motor = ESC(pin=2)
+motor1 = ESC(pin=2)
+#motor2 = ESC(pin=3)
+#motor3 = ESC(pin=4)
+#motor4 = ESC(pin=5)
 
 # armement
 print("Armement ESC...")
-motor.arm()
+motor1.arm()
+#motor2.arm()
+#motor3.arm()
+#motor4.arm()
 
 # test 100%
 print("Test 100%")
-motor.throttle(100)
-time.sleep(5)
+motor1.throttle(30)
+#motor2.throttle(10)
+#motor3.throttle(10)
+#motor4.throttle(10)
+time.sleep(2)
 
 # arrêt
-motor.stop()
+motor1.stop()
+#motor2.stop()
+#motor3.stop()
+#motor4.stop()
